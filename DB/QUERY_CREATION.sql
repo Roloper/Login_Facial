e0930dacf26220231ca0471b185af68ba69117dc
Rolo <89317868+Roloper@users.noreply.github.com>
2023-07-08 18:22:23 -0500

    R

--- DB/QUERY_CREATION.sql
+++ DB/QUERY_CREATION.sql
@@ -1,51 +1,45 @@
--- Crear tabla Venta
+CREATE TABLE Usuario (
+  id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+  Nombre VARCHAR(100),
+  correo VARCHAR(100),
+  contrasena VARCHAR(100),
+  imagen_perfil BLOB
+);
+
+CREATE TABLE Vendedor (
+  id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+  imagen_test BLOB,
+  FOREIGN KEY (id_usuario) REFERENCES Usuario (id_usuario)
+);
+
+CREATE TABLE Admin (
+  id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+  FOREIGN KEY (id_usuario) REFERENCES Usuario (id_usuario)
+);
+
 CREATE TABLE Venta (
   id_venta INT PRIMARY KEY AUTO_INCREMENT,
-  fecha INT,
-  obs INT,
+  fecha DATE,
+  obs VARCHAR(255),
   id_usuario INT,
-  FOREIGN KEY (id_usuario) REFERENCES Vendedor(id_usuario)
+  FOREIGN KEY (id_usuario) REFERENCES Vendedor (id_usuario)
 );
 
--- Crear tabla Producto
 CREATE TABLE Producto (
   id_producto INT PRIMARY KEY AUTO_INCREMENT,
-  nombre_produc INT,
-  descripcion INT,
+  nombre_produc VARCHAR(100),
+  descripcion VARCHAR(255),
   precio_uni INT,
   id_usuario INT,
-  FOREIGN KEY (id_usuario) REFERENCES Vendedor(id_usuario)
+  FOREIGN KEY (id_usuario) REFERENCES Vendedor (id_usuario)
 );
 
--- Crear tabla Detalle
 CREATE TABLE Detalle (
   id_venta INT,
-  id_producto INT,
+  id_producto INT AUTO_INCREMENT,
   cantidad INT,
   precio_total INT,
   PRIMARY KEY (id_venta, id_producto),
-  FOREIGN KEY (id_venta) REFERENCES Venta(id_venta),
-  FOREIGN KEY (id_producto) REFERENCES Producto(id_producto)
-);
-
--- Crear tabla Vendedor
-CREATE TABLE Vendedor (
-  id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-  imagen_test INT,
-  FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
-);
-
--- Crear tabla Admin
-CREATE TABLE Admin (
-  id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-  FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
-);
-
--- Crear tabla Usuario
-CREATE TABLE Usuario (
-  id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-  Nombre INT,
-  correo INT,
-  contraseña INT,
-  imagen_perfil INT
+  FOREIGN KEY (id_venta) REFERENCES Venta (id_venta),
+  FOREIGN KEY (id_producto) REFERENCES Producto (id_producto)
 );
