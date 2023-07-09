3a6ec27e3a8d1a1532a83ff91cbb90814c38ee8e
bejamin11 <129314432+bejamin11@users.noreply.github.com>
2023-07-08 22:47:56 -0500

    detalle

---
+++ models/entities/Detalle.py
@@ -0,0 +1,7 @@
+class Detalle():
+
+    def __init__(self, id_venta, id_producto, cantidad , precio_total):
+        self.id_venta = id_venta
+        self.id_producto = id_producto
+        self.cantidad  = cantidad 
+        self.precio_total = precio_total
