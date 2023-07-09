5a1a19244498a4789f975571f8d62d04da6524e0
Rodri123zxc <129563315+Rodri123zxc@users.noreply.github.com>
2023-07-09 17:22:08 -0500

    Update facial.py

--- Facial/facial.py
+++ Facial/facial.py
@@ -1,55 +1,71 @@
 import cv2
 import os
-import numpy as np
-import mysql.connector
-from config import config
+from Facial.entrenandoRF import train_face_recognizer
 
-development_config = config['development']
+cap = cv2.VideoCapture(0)
+face_detector = cv2.CascadeClassifier(cv2.data.haarcascades +
+     "haarcascade_frontalface_default.xml")
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('modeloLBPHFace.xml')
+dataPath = './Data'
+imagePaths = os.listdir(dataPath)
+nom = ''
 
-conexion = mysql.connector.connect(
-    host=development_config.MYSQL_HOST,
-    user=development_config.MYSQL_USER,
-    password=development_config.MYSQL_PASSWORD,
-    database=development_config.MYSQL_DB
-)
+def generate():
+    imagePaths = os.listdir(dataPath)
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            auxFrame = gray.copy()
+            faces = face_detector.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                rostro = auxFrame[y:y + h, x:x + w]
+                rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
+                result = face_recognizer.predict(rostro)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            if imagePaths and isinstance(result[0], int) and 0 <= result[0] < len(imagePaths):
+                cv2.putText(frame, '{}'.format(imagePaths[result[0]]), (x, y - 25), 2, 1.1, (0, 255, 0), 1,
+                            cv2.LINE_AA)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            else:
+                cv2.putText(frame, 'Desconocido', (x, y - 20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-def train_face_recognizer():
+            (flag, encodedImage) = cv2.imencode(".jpg", frame)
+            if not flag:
+                continue        
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                   bytearray(encodedImage) + b'\r\n')
+
+
+def register(personName):
+    print("inicio register")
     dataPath = './Data'
-    peopleList = os.listdir(dataPath)
-    print('Lista de personas:', peopleList)
-
-    labels = []
-    facesData = []
-    label = 0
-
-    for nameDir in peopleList:
-        personPath = dataPath + '/' + nameDir
-        print('Leyendo las imágenes')
-
-        for fileName in os.listdir(personPath):
-            print('Rostros:', nameDir + '/' + fileName)
-            labels.append(label)
-            facesData.append(cv2.imread(personPath + '/' + fileName, 0))
-        label = label + 1
-
-    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-    print("Entrenamiento")
-    face_recognizer.train(facesData, np.array(labels))
-
-    # Serializar el modelo en un objeto de bytes
-    modeloBytes = cv2.face.LBPHFaceRecognizer_create()
-    face_recognizer.write('modelo.xml')
-    face_recognizer.save('modeloLBPHFace.xml')
-
-    # Guardar el modelo en la tabla Usuario
-    cursor = conexion.cursor()
-    with open('modeloLBPHFace.xml', 'rb') as archivo:
-        modeloBytes = archivo.read()
-
-    cursor.execute("UPDATE Usuario SET modelo = %s", (modeloBytes,))
-    conexion.commit()
-
-    print("Modelo Listo")
-
-conexion.close()
+    personPath = dataPath + '/' + personName
+    if not os.path.exists(personPath):
+        print('Carpeta creada:', personPath)
+        os.makedirs(personPath)
+    faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    count = 0
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            auxFrame = frame.copy()
+            faces = face_detector.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                rostro = auxFrame[y:y + h, x:x + w]
+                rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
+                cv2.imwrite(personPath + '/rostro_{}.jpg'.format(count), rostro)
+                count = count + 1
+            (flag, encodedImage) = cv2.imencode(".jpg", frame)
+            if not flag:
+                continue
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                   bytearray(encodedImage) + b'\r\n')
+            k = cv2.waitKey(1)
+            if k == 27 or count >= 300:
+                break
+    train_face_recognizer()
