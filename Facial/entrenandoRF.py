b7b5e45cdd5750d9da44b4314fcaf5d7ebbc3796
Rolo <89317868+Roloper@users.noreply.github.com>
2023-07-09 17:57:10 -0500

    R

--- Facial/entrenandoRF.py
+++ Facial/entrenandoRF.py
@@ -1,17 +1,17 @@
 import cv2
 import os
 import numpy as np
-import mysql.connector
+#import mysql.connector
 from config import config
 
 development_config = config['development']
 
-conexion = mysql.connector.connect(
-    host=development_config.MYSQL_HOST,
-    user=development_config.MYSQL_USER,
-    password=development_config.MYSQL_PASSWORD,
-    database=development_config.MYSQL_DB
-)
+# conexion = mysql.connector.connect(
+#     host=development_config.MYSQL_HOST,
+#     user=development_config.MYSQL_USER,
+#     password=development_config.MYSQL_PASSWORD,
+#     database=development_config.MYSQL_DB
+# )
 
 def train_face_recognizer():
     dataPath = './Data'
@@ -44,8 +44,8 @@ def train_face_recognizer():
         modeloBytes = archivo.read()
 
     cursor.execute("UPDATE Usuario SET imagen_test = %s WHERE id_usuario = 1", (modeloBytes,))
-    conexion.commit()
+    # conexion.commit()
 
     print("Modelo Listo")
 
-conexion.close()
+# conexion.close()
--- models/entities/Usuario.py
+++ models/entities/Usuario.py
@@ -3,12 +3,14 @@ from flask_login import UserMixin
 
 class User(UserMixin):
 
-    def __init__(self, id_usuario, nombre, correo, password, imagen_perfil=None,imagen_test=None):
+    def __init__(self, id_usuario, nombre, correo, password,admin, imagen_perfil=None,imagen_test=None):
         self.id_usuario = id_usuario
         self.nombre = nombre
         self.correo = correo
         self.password = password
+        self.admin = admin
         self.imagen_perfil = imagen_perfil
+        self.imagen_test = imagen_test
 
     @classmethod
     def check_password(self, hashed_password, password):
