74f74c075661591dc9ede9e346c6cb1ba52ecca6
Rodri123zxc <129563315+Rodri123zxc@users.noreply.github.com>
2023-07-12 18:00:30 -0500

    a

--- Facial/entrenandoRF.py
+++ Facial/entrenandoRF.py
@@ -1,6 +1,51 @@
 import cv2
 import os
 import numpy as np
+#import mysql.connector
+from config import config
+from models.ModelUser import ModelUser
+from models.entities.Usuario import User
+
+development_config = config['development']
+
+def train_face_recognizer():
+    dataPath = './Data'
+    peopleList = os.listdir(dataPath)
+    peopleList = ModelUser.get_all_users() 
+    print('Lista de personas:', peopleList)
+
+    labels = []
+    facesData = []
+    label = 0
+
+    for nameDir in peopleList:
+        personPath = dataPath + '/' + nameDir
+        print('Leyendo las imágenes')
+
+        for fileName in os.listdir(personPath):
+            print('Rostros:', nameDir + '/' + fileName)
+            labels.append(label)
+            facesData.append(cv2.imread(personPath + '/' + fileName, 0))
+        label = label + 1
+
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    print("Entrenamiento")
+    face_recognizer.train(facesData, np.array(labels))
+
+    face_recognizer.save('modeloLBPHFace.xml')
+    with open('modeloLBPHFace.xml', 'rb') as archivo:
+        modeloBytes = archivo.read()
+
+    print("Modelo Listo")
+
+    face_recognizer.write('modeloLBPHFace.xml')
+    print("Modelo Listo")
+
+"""
+import cv2
+import os
+import numpy as np
 from config import config
 from models.ModelUser import ModelUser
 from app import db
@@ -30,52 +75,4 @@ def train_face_recognizer():
 train_face_recognizer()
 
 #Conclusion esta casi igual, a diferencia de label+1, y que no se q ponerle en los parametros de cls, y db
-
-""""
-import cv2
-import os
-import numpy as np
-#import mysql.connector
-from config import config
-from models.ModelUser import ModelUser
-from models.entities.Usuario import User
-
-development_config = config['development']
-
-def train_face_recognizer():
-    #dataPath = './Data'
-    #peopleList = os.listdir(dataPath)
-    peopleList = ModelUser.get_all_users() # peopleList = users
-    print('Lista de personas:', peopleList)
-
-    labels = []
-    facesData = []
-    label = 0
-
-    for nameDir in peopleList:
-        #personPath = dataPath + '/' + nameDir
-        print('Leyendo las imágenes')
-
-        for fileName in os.listdir(personPath):
-            print('Rostros:', nameDir + '/' + fileName)
-            labels.append(label)
-            facesData.append(cv2.imread(personPath + '/' + fileName, 0))
-        label = label + 1
-
-    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-    print("Entrenamiento")
-    face_recognizer.train(facesData, np.array(labels))
-
-
-    # Guardar el modelo en la columna imagen_test de la tabla Usuario
-    #cursor = conexion.cursor()
-    face_recognizer.save('modeloLBPHFace.xml')
-    with open('modeloLBPHFace.xml', 'rb') as archivo:
-        modeloBytes = archivo.read()
-
-    print("Modelo Listo")
-
-# conexion.close()
-    face_recognizer.write('modeloLBPHFace.xml')
-    print("Modelo Listo")"""
+"""
